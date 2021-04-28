615fe8c464f0dc438a623a489a7b98f4d3816561
tfelic3 <felicien.tesalon@gmail.com>
2021-04-28 16:08:19 -0400

    update

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("imageSlides");
+  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
